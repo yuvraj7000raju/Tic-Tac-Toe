3f4bbe8f96b5806a018ecceb4d821f1ba9e80fb3
yuvraj yadav <yuvraj7000raju@gmail.com>
2023-08-11 13:58:38 +0530

    background logic is completed

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
+function addimage(m, n, row) {
+    return function () {
+        let index = n-1;
+        let last = result[-1]
+        if (rows[index] === 1 && length == false) {
+            const pic = document.createElement("img")
+            if (p == 0) {
+                pic.src = "cross.png"
+                p = 1
+                arr[index] = 1;
+                rows[index]= 0;
+            }
+            else {
+                pic.src = "zero.png"
+                p = 0
+                arr[index] = 0;
+                rows[index] = 0;
+            }
+            // winner.backgroundColor = "blue";
+            m.innerHTML = "";
+            m.appendChild(pic)
+            console.log(n)
+            console.log(arr[index])
+            result.push(n)
+            // check(index , n)
+            for(let i = 0; i<9; i++){
+                if(i == 0 || i == 3|| i ==6){
+                    length = arr[i]==arr[i+1] ? arr[i]==arr[i+2] ? true : false : false
+                    if(length == true){
+                        console.log("winner")
+                        break
+                    }
+                }
+                if( i==0 ||i ==1 || i==2){
+                length = arr[i]==arr[i+3] ? arr[i]== arr[i+6] ? true :false :false
+                if(length == true){
+                    console.log("winner")
+                    break
+                }
+               }
+               if(i==0){
+                   length = arr[i]==arr[i+4] ? arr[i] == arr[i+8] ? true : false : false
+                if(length == true){
+                    console.log("winner")
+                    break
+                }
+               }
+               if(i == 2){
+                length = arr[i]==arr[i+2] ? arr[i] == arr[i+4] ? true : false : false
+                if(length == true){
+                    console.log("winner")
+                    break
+                }
+               }
+            }
+        }
+        else {
+            console.log("the game is already over")
+        }
+        if(length == true){
+            let boss = (p ==0) ? "0" : "x" ;
+            console.log("!!!",boss, " is winner !!!")
+        }
+    }
+    
+}
 let p = 0
 // winner.style.backgroundColor = "blue";
-let arr = [10, 10, 10, 10, 10, 10, 10, 10, 10]
-let result =[]
-let length = 0;
+let arr = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+let rows = [1,1,1,1,1,1,1,1,1]
+let result = []
+let length = false;
 let winner = document.getElementsByClassName("winner")
 let a11 = document.getElementById("b11")
 let a12 = document.getElementById("b12")
@@ -13,49 +80,12 @@ let a23 = document.getElementById("b23")
 let a31 = document.getElementById("b31")
 let a32 = document.getElementById("b32")
 let a33 = document.getElementById("b33")
-function check(index){
-    return function(){
-    if(arr[index]===arr[index+1]===arr[index+2] || arr[index]===arr[index-1]===arr[index-2]  || arr[index-1]===arr[index]===arr[index+1]){
-        winner.style.backgroundColor = "blue";
-        console.log("winner")
-        arr = null
-    }
-}
-
-}
-function addimage(m, n, index) {
-    return function () {
-            if (arr[index]=== 10) {
-            const pic = document.createElement("img")
-            if (p == 0) {
-                pic.src = "cross.png"
-                p = 1
-                arr[index] = 1;
-            }
-            else {
-                pic.src = "zero.png"
-                p = 0
-                arr[index] = 0;
-            }
-            m.innerHTML = "";
-            m.appendChild(pic)
-            console.log(n)
-            console.log(arr[index])
-            result.push(n)
-            length++;
-            check(index)
-        }
-    else{
-          console.log("hello")
-    }}
-
-}
-a11.addEventListener("click", addimage(a11, 1, 0))
+a11.addEventListener("click", addimage(a11, 1, 1))
 a12.addEventListener("click", addimage(a12, 2, 1))
-a13.addEventListener("click", addimage(a13, 3, 2))
-a21.addEventListener("click", addimage(a21, 4, 3))
-a22.addEventListener("click", addimage(a22, 5, 4))
-a23.addEventListener("click", addimage(a23, 6, 5))
-a31.addEventListener("click", addimage(a31, 7, 6))
-a32.addEventListener("click", addimage(a32, 8, 7))
-a33.addEventListener("click", addimage(a33, 9, 8))
+a13.addEventListener("click", addimage(a13, 3, 1))
+a21.addEventListener("click", addimage(a21, 4, 2))
+a22.addEventListener("click", addimage(a22, 5, 2))
+a23.addEventListener("click", addimage(a23, 6, 2))
+a31.addEventListener("click", addimage(a31, 7, 3))
+a32.addEventListener("click", addimage(a32, 8, 3))
+a33.addEventListener("click", addimage(a33, 9, 3))
